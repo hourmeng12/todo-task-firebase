ecb5aebc6f6f7f423ac978c428a7a5c638fb4653
Hourmeng Khy <itshourmeng@gmail.com>
2022-06-24 18:14:41 +0700

    Add hover-state to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,16 +31,19 @@ const Navbar = () => {
       <div className="fixed top-0 left-0 z-40 flex w-full items-center justify-between bg-white/50 p-2 text-zinc-700 backdrop-blur-sm transition-all duration-200 dark:bg-zinc-900/50 dark:text-zinc-200">
         {user && (
           <div>
-            <Button onClick={toggleMenu}>
+            <Button
+              className="hover:bg-zinc-100 dark:hover:bg-zinc-800"
+              onClick={toggleMenu}
+            >
               <MenuIcon className="h-5 w-5" />
             </Button>
           </div>
         )}
         <Link
           to="/"
-          className="inline-flex h-10 min-w-[2.5rem] items-center justify-center"
+          className="group inline-flex h-10 min-w-[2.5rem] items-center justify-center"
         >
-          <CollectionIcon className="mr-1 h-5 w-5 stroke-[1.5]" />
+          <CollectionIcon className="mr-1 h-5 w-5 stroke-[1.5] transition-transform duration-200 group-hover:scale-110" />
           <h1 className="text-lg font-medium">Todo</h1>
         </Link>
         <div>
